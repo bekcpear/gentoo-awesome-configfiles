d830553bd21adb43cde2a2471bfde2518a23a53e
Bekcpear <ink2.0plus@gmail.com>
2016-10-09 18:26:24 +0800

    1.devilspie-->devilspie2 & 2.配置sakura terminal 以配合musicbox播放音乐，因为musicbox在xterm下播放闪烁无法解决

---
+++ home/royuz/.config/devilspie2/1.lua
@@ -0,0 +1,6 @@
+if(get_window_class() == "XTerm") then
+  set_window_opacity(0.7);
+elseif(get_window_class() == "Sakura") then
+  set_window_opacity(0.7);
+  set_window_geometry2(526,20,1394,362);
+end
